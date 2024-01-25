4283ac141d7eebfea58a0cc4675be69a62342916
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-01-25 22:51:18 +0600

    added

---
+++ csharp-b16-exam1/src/Exam1/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Task 2");
+
+
+int number = int.Parse(Console.ReadLine());
+
+int leftShift = number << 3;
+
+Console.WriteLine($"before: {Convert.ToString(number,2)}");
+Console.WriteLine($"After: {Convert.ToString(leftShift,2)}");
---
+++ csharp-b16-exam1/src/Exam1/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Task 3");
+
+decimal n1 = decimal.Parse(Console.ReadLine());
+decimal n2 = decimal.Parse(Console.ReadLine());
+decimal n3 = decimal.Parse(Console.ReadLine());
+
+decimal avg = (n1 + n2 + n3) / 3;
+Console.WriteLine(avg);
+
