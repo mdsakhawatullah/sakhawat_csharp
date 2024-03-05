161a15d2ca29236587e8e9a1ad7f099ca760a7a7
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-05 21:05:22 +0600

    new problem solution added

---
+++ src/problem_10_122A/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace problem_10_122A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int inputNumber = int.Parse(Console.ReadLine());
+            if(inputNumber%4==0 || inputNumber%7==0 || inputNumber%47==0 || inputNumber % 77 == 0 || inputNumber % 444 == 0
+                || inputNumber % 447 == 0 || inputNumber % 474 == 0 || inputNumber % 477 == 0 || inputNumber % 777 == 0
+                || inputNumber % 774 == 0 || inputNumber % 747 == 0 ) 
+            {   
+                count++;
+            }
+            if (count > 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+
+        }
+    }
+}
