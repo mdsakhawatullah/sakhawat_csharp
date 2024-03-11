fd710413e91bdd1cd4734cfc971bfcbd419d2096
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-11 19:01:04 +0600

    task file added

---
+++ src/Exam2/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new Random(DateTime.Now.Millisecond);
+
+for(int i = 0; i < 10; i++)
+    Console.WriteLine(GetRandomChar());
+
+char GetRandomChar()
+{
+    return (char)random.Next('A', 'Z');
+}
