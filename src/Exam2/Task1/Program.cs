6ec26991508214dbbd6781c8c300f795e44453f1
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-11 19:43:17 +0600

    task1 added

--- src/Exam2/Task1/Program.cs
+++ src/Exam2/Task1/Program.cs
@@ -1,9 +1,39 @@
-﻿Random random = new Random(DateTime.Now.Millisecond);
+﻿using System;
+using System.IO;
+using System.Text;
 
-for(int i = 0; i < 10; i++)
-    Console.WriteLine(GetRandomChar());
+long sizeGB = 5;
+long sizeBytes = sizeGB * 1024 * 1024 * 1024;
 
-char GetRandomChar()
+string filePath = "../../../../demo.txt";
+long fileInBytes = TextGeneratorFile(filePath, sizeBytes);
+
+Console.WriteLine(sizeBytes);
+if (fileInBytes == sizeBytes) Console.WriteLine("done");
+
+
+static long TextGeneratorFile(string filePath, long sizeBytes)
+
+{
+    using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8))
+    {
+        Random random = new Random(DateTime.Now.Millisecond);
+        long fileInBytes = 0;
+        while (fileInBytes < sizeBytes)
+        {
+            char randomChar = GetRandomChar(random);
+            writer.Write(randomChar);
+            fileInBytes += sizeof(char);
+        }
+        return fileInBytes;
+    }
+}
+
+
+/*for (int i = 0; i < 10; i++)
+    Console.WriteLine(GetRandomChar(random));*/
+
+static char GetRandomChar(Random random)
 {
     return (char)random.Next('A', 'Z');
 }
