878cfad4241768da9394647af819f6c3c212fc80
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-11 23:07:18 +0600

    new

--- src/Exam2/Task2/Program.cs
+++ src/Exam2/Task2/Program.cs
@@ -7,8 +7,7 @@ string filePath = "../../../../demo.txt";
 int sizeMB = 100;
 long sizeInBytes = sizeMB * 1024 * 1024;
 
-int folderCount = 1;
-string folderPath = $"Folder{folderCount}";
+string folderPath = $"Folder1";
 Directory.CreateDirectory(folderPath);
 
 using (FileStream fileStream = File.OpenRead(filePath))
@@ -16,25 +15,6 @@ using (FileStream fileStream = File.OpenRead(filePath))
     byte[] bytes = new byte[sizeMB];
     int bytesRead = 0;
     long totalRead = 0;
-
-    while ((bytesRead = fileStream.Read(bytes, 0, bytes.Length)) > 0)
-    {
-        string sizeMBFilePath = Path.Combine(folderPath, $"smallTextFile{folderCount}.txt");
-        using (FileStream sizeMBFileStream = File.Create(sizeMBFilePath))
-        {
-            sizeMBFileStream.Write(bytes, 0, bytesRead);
-        }
-
-        totalRead += bytesRead;
-
-
-        if (totalRead > sizeInBytes)
-        {
-            folderCount++;
-            folderPath = $"Folder{folderCount}";
-        }
-
-    }
 
 }
 Console.WriteLine("done");
