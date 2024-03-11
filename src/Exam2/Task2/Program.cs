93ced7208a1fa2ef8467b0a4d0b070ce34a1527a
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-11 20:19:16 +0600

    task 2 added

--- src/Exam2/Task2/Program.cs
+++ src/Exam2/Task2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+string filePath = "../../../../demo.txt";
+int sizeMB = 100;
+long sizeInBytes = sizeMB * 1024 * 1024;
+
+int folderCount = 1;
+string folderPath = $"Folder{folderCount}";
+Directory.CreateDirectory(folderPath);
+
+using (FileStream fileStream = File.OpenRead(filePath))
+{
+    byte[] bytes = new byte[sizeMB];
+    int bytesRead = 0;
+    long totalRead = 0;
+
+    while ((bytesRead = fileStream.Read(bytes, 0, bytes.Length)) > 0)
+    {
+        string sizeMBFilePath = Path.Combine(folderPath, $"smallTextFile{folderCount}.txt");
+        using (FileStream sizeMBFileStream = File.Create(sizeMBFilePath))
+        {
+            sizeMBFileStream.Write(bytes, 0, bytesRead);
+        }
+
+        totalRead += bytesRead;
+
+
+        if (totalRead > sizeInBytes)
+        {
+            folderCount++;
+            folderPath = $"Folder{folderCount}";
+        }
+
+    }
+
+}
+Console.WriteLine("done");
