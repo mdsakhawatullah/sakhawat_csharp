75ce34df21415e1ea20747d3f78754173b548a85
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-19 23:14:39 +0600

    new file added

---
+++ src/problem_65_732A/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace problem_65_732A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int k = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+            int count = 0;
+
+            for (int i = 1; ; i++)
+            {
+                count += k;
+                if (count % 10 == 0 || count % 10 == n)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else continue;
+            }
+                
+        }
+    }
+}
