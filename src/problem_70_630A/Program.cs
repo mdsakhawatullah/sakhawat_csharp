04048a496125a92ae099109eb1722b77402921c8
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-03-21 22:33:24 +0600

    new file added

---
+++ src/problem_70_630A/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace problem_70_630A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            
+
+            Console.WriteLine(25);
+        }
+    }
+}
