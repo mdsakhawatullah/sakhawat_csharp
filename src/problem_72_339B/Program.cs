b6225dc8a6e3322eb8f6a81e3651fcc50bcbba58
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-04-04 00:06:10 +0600

    new file add

---
+++ src/problem_72_339B/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace problem_72_339B
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split(' ');
+            int n = int.Parse(num[0]);
+            int m = int.Parse(num[1]);
+
+            int[] arr = new int[m];
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            for(int i=0; i<m; i++)
+            {
+                arr[i] = int.Parse(input[i]);
+            }
+
+            long count = arr[0] - 1;
+
+            for(int i=0; i<m-1; i++)
+            {
+                if (arr[i] > arr[i+1])
+                {
+                    count += n - (arr[i] - arr[i + 1]);
+                }
+                else if (arr[i+1] > arr[i])
+                {
+                    count += arr[i + 1] - arr[i];
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ src/problem_73_451A/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace problem_73_451A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            int count = 0;
+
+            for(int i=1; ; i++)
+            {
+                if (n - i == 0) break;
+                else if (m - i == 0) break;
+                count++;
+            }
+
+            if(count%2 == 0) { Console.WriteLine("Akshat"); }
+            else { Console.WriteLine("Malvika"); }
+        }
+    }
+}
