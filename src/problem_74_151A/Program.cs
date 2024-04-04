78d14b82588a4f6b7b671421a721850055f90911
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-04-04 23:54:07 +0600

    new file added

---
+++ src/problem_74_151A/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace problem_74_151A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+            int l = int.Parse(input[2]);
+            int c = int.Parse(input[3]);
+            int d = int.Parse(input[4]);
+            int p = int.Parse(input[5]);
+            int nl = int.Parse(input[6]);
+            int np = int.Parse(input[7]);
+
+            int totalDrinks = k * l;
+            totalDrinks = totalDrinks / nl;
+            int totalSlice = c * d;
+            int totalSalt = p / np;
+
+            int srt = Math.Min(totalDrinks, totalSlice);
+
+            int count = (Math.Min(srt, totalSalt))/n;
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ src/problem_75_131A/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace problem_75_131A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+            int c = 0, d = 0;
+
+            for(int i=1; i<chars.Length; i++)
+            {
+
+                if (chars[i] == char.ToLower(chars[i]))
+                {
+                    c++;
+                }
+            }
+            for(int i=0; i<chars.Length; i++)
+            {
+                if (chars[i] == char.ToUpper(chars[i]))
+                {
+                    d++;
+                }
+            }
+            if (chars[0] == char.ToLower(chars[0]) && c==0)
+            {
+                chars[0] = char.ToUpper(chars[0]);
+                for(int i=1; i<chars.Length; i++)
+                {
+                    chars[i] = char.ToLower(chars[i]);
+                }
+                string result = new string(chars);
+                Console.WriteLine(result);
+                return;
+            }
+            if(d== chars.Length)
+            {
+                for(int i=0; i<chars.Length;i++)
+                {
+                    chars[i] = char.ToLower(chars[i]);
+                }
+
+                string result = new string(chars);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                string result = new string(chars);
+                Console.WriteLine(result);
+            }
+
+          
+        }
+    }
+}
